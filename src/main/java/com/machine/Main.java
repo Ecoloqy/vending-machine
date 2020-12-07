3276160a9eefcdd5c0e8da8340e131e6cc0923ce
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-12-07 21:09:59 +0100

    Add throwing exception when running program in that state

--- src/main/java/com/machine/Main.java
+++ src/main/java/com/machine/Main.java
@@ -1,5 +1,9 @@
 package com.machine;
 
-public class Main {
+import com.machine.handlers.InvalidOperationException;
 
+public class Main {
+    public static void main(String[] args) throws InvalidOperationException {
+        throw new InvalidOperationException("Not implemented yet!");
+    }
 }
