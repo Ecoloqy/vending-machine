e7e814e6c309b14de214bac30b2fc08c42d219d2
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-12-08 09:37:29 +0100

    Add new collection of coins inside vending machine holding all coins from which machine can return coins, and Add three helpful methods to handle purchase operations

--- src/main/java/com/machine/VendingMachine.java
+++ src/main/java/com/machine/VendingMachine.java
@@ -10,30 +10,53 @@ import java.util.Map;
 
 public class VendingMachine {
     private final Map<Product, Integer> products;
-    private final Map<Coin, Integer> coinsInVendingMachine = new HashMap<>();
-    private final Map<Coin, Integer> coinReturn = new HashMap<>();
+    private final Map<Coin, Integer> coinsInVendingMachine;
+    private final Map<Coin, Integer> coinsInBuyInMode = new HashMap<>();
+    private final Map<Coin, Integer> coinsReturn = new HashMap<>();
     private String display;
+    private boolean isPurchaseInProgress = false;
 
-    public VendingMachine(Map<Product, Integer> products) {
+    public VendingMachine(Map<Product, Integer> products, Map<Coin, Integer> coinsInVendingMachine) {
         this.products = products;
+        this.coinsInVendingMachine = coinsInVendingMachine;
         this.display = Display.MACHINE_EMPTY.getMessage();
     }
 
     public String getDisplay() {
+        if (isPurchaseInProgress) isPurchaseInProgress = false;
         return display;
     }
 
     public void insertCoin(Coin coin) {
-        if (coin.equals(Coin.PENNY)) addCoinToContainer(coin, coinReturn);
-        else addCoinToContainer(coin, coinsInVendingMachine);
+        if (coin.equals(Coin.PENNY)) addCoinToContainer(coin, coinsReturn);
+        else addCoinToContainer(coin, coinsInBuyInMode);
     }
 
     public Map<Coin, Integer> getCoinReturn() {
-        return coinReturn;
+        return coinsReturn;
     }
 
     public Product selectProduct(Product product) {
-        return product;
+        isPurchaseInProgress = true;
+        if (purchaseCanBeDone()) {
+            doPurchaseOfProduct();
+            display = Display.PURCHASE_DONE.getMessage();
+            return product;
+        }
+        display = calculateNeededSumOfCoinsAndSetUpMessage(product);
+        return null;
+    }
+
+    private boolean purchaseCanBeDone() {
+        return true;
+    }
+
+    private void doPurchaseOfProduct() {
+
+    }
+
+    private String calculateNeededSumOfCoinsAndSetUpMessage(Product product) {
+        return "";
     }
 
     private void addCoinToContainer(Coin coin, Map<Coin, Integer> container) {
@@ -44,7 +67,7 @@ public class VendingMachine {
 
     private void updateMoneyOnDisplay() {
         BigDecimal totalCost = BigDecimal.ZERO;
-        for (Map.Entry<Coin, Integer> entry : coinsInVendingMachine.entrySet()) {
+        for (Map.Entry<Coin, Integer> entry : coinsInBuyInMode.entrySet()) {
             BigDecimal itemCost = entry.getKey().getValue().multiply(new BigDecimal(entry.getValue()));
             totalCost = totalCost.add(itemCost);
         }
